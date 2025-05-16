071b2abc64de1f591b974570dca7574fe39f0d98
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2025-05-16 10:17:21 +0700

    dashboard admin temporary

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,7 +16,7 @@ module.exports = {
         if (req.session.user && req.session.user.role === 'admin') {
             return next(); // Akses diizinkan, lanjutkan ke rute berikutnya
         }
-        return res.redirect('/loginPage'); // Akses ditolak, redirect ke halaman login
+        return res.redirect('/loginAdmin'); // Akses ditolak, redirect ke halaman login
     }, isAlumni: (req, res, next) => {
         if (req.session.user && req.session.user.role === 'alumni') {
             return next(); // Akses diizinkan, lanjutkan ke rute berikutnya
