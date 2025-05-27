364c53946a56c00868805a3f3de365cd7d3ef03e
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2025-05-27 13:03:53 +0700

    feat: enhance admin schema with unique email constraint and password hashing

--- models/admin.js
+++ models/admin.js
@@ -1,21 +1,32 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const adminSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
     },
     password: {
         type: String,
         required: true
-
     },
     nama: {
         type: String,
         optional: true,
         default: null
     },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 }, { timestamps: true });
+
+adminSchema.pre('save', async function (next) {
+    // Hanya hash password jika password field dimodifikasi (baru dibuat atau diganti)
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
 
 const Admin = mongoose.model('Admin', adminSchema);
 module.exports = Admin;
